aede3e533b0e18f61b1d68bdde2ff5964e0c42fe
seenso <>
2021-12-02 14:01:08 -0900

    rendering all listings

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
+const api_url = "http://localhost:6001/listings";
+
 function App() {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch(api_url)
+    .then(r => r.json())
+    .then(data => setListings(data));
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,14 @@
 import React from "react";
-// import ListingCard from "./ListingCard";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({listings}) {
   return (
     <main>
       <ul className="cards">
         {/* use the ListingCard component to display listings */}
+        {listings.map(l => (
+          <ListingCard listing={l}/>
+        ))}
       </ul>
     </main>
   );
